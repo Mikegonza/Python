a5118415e6f3c3204949decb77f873da9aa439ab
Mikegonza <miguelangel.gonzalezmaur@my.tccd.edu>
2025-04-29 21:56:52 -0500

    minute 36

---
+++ Phyton/practice_full_course.py
@@ -0,0 +1,43 @@
+#https://www.youtube.com/watch?v=rfscVS0vtbw&t=5055s&ab_channel=freeCodeCamp.org 
+print("hello world!")
+print("    *")
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
+print("\n")
+#Print statement 
+character_name = "Miguel"
+character_age = "40"
+print("There was a man named " + character_name + ", ")
+#updating the same variable 
+character_name = "For his last name"
+print("He was "+ character_age + " years old. ")
+print("but did not like beign " + character_age + ".")
+print("but did not like call " + character_name + ".")
+#Strings \n next character to a new line.
+#\ escape character to include a " or ' in the text
+#creating a variable string
+phrase = "Type:\n"
+#concatanation with + sign
+print(phrase + "World\n\'wide\n\'web")
+#using functions
+phrase = "live long and prosper:"
+print(phrase.lower())
+print(phrase.islower())
+print(phrase.upper())
+print(phrase.isupper())
+#get the length of a string start from 0
+print(len(phrase))
+#get the value of first character
+print(phrase[0])
+
+
+
+
+
+
+
+
+
+
