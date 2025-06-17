51a38271f6d8ad2eb0519f086a54c71b63272af3
Miguel Gonzalez <miguelgonzalez404@outlook.com>
2025-06-16 22:09:40 -0500

    working with swap

--- Phyton/practice_full_course.py
+++ Phyton/practice_full_course.py
@@ -563,3 +563,67 @@ elif choice == 2:
     print_figure(print_human_head)
 '''
 
+'''#List argument modification.
+#Address the FIXME comments. Move the respective code from the while-loop to the created function.
+#The add_grade function has already been created.
+#Note: split() and strip() are string methods further explained elsewhere. 
+#split() separates a string into tokens using any whitespace as the default separator. 
+#The tokens are returned in a list (i.e., 'a b c'.split() returns ['a', 'b', 'c']). 
+#strip() returns a copy of a string with leading and trailing whitespace removed.
+
+def add_grade(student_grades):
+    print('Entering grade. \n')
+    name, grade = input(grade_prompt).split()
+    student_grades[name] = grade
+
+# FIXME: Create delete_name function
+def delete_name():
+    print('Deleting grade.\n')
+    name = input(delete_prompt)
+    del student_grades[name]
+
+# FIXME: Create print_grades function
+def print_grades():
+    print('Printing grades.\n')
+    for name, grade in student_grades.items():
+            print(name, 'has a', grade)
+
+student_grades = {}  # Create an empty dict
+grade_prompt = "Enter name and grade (Ex. 'Bob A+'):\n"
+delete_prompt = "Enter name to delete:\n"
+menu_prompt = ("1. Add/modify student grade\n"
+                "2. Delete student grade\n"
+                "3. Print student grades\n"
+                "4. Quit\n\n")
+
+command = input(menu_prompt).lower().strip()
+
+while command != '4':  # Exit when user enters '4'
+    if command == '1':
+        add_grade(student_grades)
+    elif command == '2':
+        # FIXME: Only call delete_name() here
+        delete_name()        
+        
+    elif command == '3':
+        # FIXME: Only call print_grades() here
+        print_grades()
+        
+    else:
+        print('Unrecognized command.\n')
+
+    command = input().lower().strip()
+'''
+
+#Write a function swap that swaps the first and last elements of a list argument.
+
+#Sample output with input: 'all,good,things,must,end,here'
+#['here', 'good', 'things', 'must', 'end', 'all']
+
+def swap(lst):
+    print(lst[-1])# starts indexin from the back of the list
+
+values_list = input().split(',')  # Program receives comma-separated values like 5,4,12,19
+swap(values_list)
+
+print(values_list)
