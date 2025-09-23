6878e11833e814c2ef3d264184beace2ca6e6833
Miguel Gonzalez <miguelgonzalez404@outlook.com>
2025-09-22 22:07:24 -0500

    working

--- 100_Days_Of_Python/Day3.py
+++ 100_Days_Of_Python/Day3.py
@@ -73,4 +73,15 @@ if height >= 120:
     print(f"Your final bill is {bill}")
 else:
     print("Sorry you have to grow taller before you can ride")
-'''    
+'''  
+
+print("Welcome to pizza deliveries!")
+size = input("What size pizza do you want? S, M or L: ")
+pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
+extra_cheese = input("Do you want extra chesse? Y or N: ")
+
+# todo: work out how much they need to pay based on their size choice.
+
+# todo: work out how much to add to their bill based on their pepperoni choice.
+
+# todo: work out their final amount based on wheather if they want extra chesse.
