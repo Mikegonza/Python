d81efc0de54f3b793858519d53b550eac32c801f
Miguel Gonzalez <miguelgonzalez404@outlook.com>
2025-09-24 20:56:59 -0500

    day 3 working

--- 100_Days_Of_Python/Day3.py
+++ 100_Days_Of_Python/Day3.py
@@ -75,13 +75,35 @@ else:
     print("Sorry you have to grow taller before you can ride")
 '''  
 
+'''Pizza selection
 print("Welcome to pizza deliveries!")
 size = input("What size pizza do you want? S, M or L: ")
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
 extra_cheese = input("Do you want extra chesse? Y or N: ")
 
 # todo: work out how much they need to pay based on their size choice.
+bill=0
+if size=="S" or "s":
+    bill+=15
+elif size=="M" or "m":
+    bill+=20
+elif size=="L" or "l":
+    bill+=25
+else:
+    print("You typed the wrong inputs.")
 
 # todo: work out how much to add to their bill based on their pepperoni choice.
+if pepperoni=="Y":
+    if size=="S":
+        bill+=2
+    else:
+        bill+=3
 
 # todo: work out their final amount based on wheather if they want extra chesse.
+if extra_cheese=="Y":
+    bill+=1
+
+print(f"Your final bill is: ${bill}.")
+
+'''
+
