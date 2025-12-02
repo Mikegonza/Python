7ec8d4b06cd67ed2c055f8e8986c9278c1f123e7
Miguel Gonzalez <miguelgonzalez404@outlook.com>
2025-12-01 20:22:37 -0600

    branches

--- Phyton/practice_full_course.py
+++ Phyton/practice_full_course.py
@@ -1366,3 +1366,21 @@ else:
     print('Another number of musicians')
 '''
 
+'''#Detecting ranges with branches(operator chaining)
+#Variable user_grade is read from input. Use operator chaining to complete the if-else expression as follows:
+#If the value of user_grade is between 9 and 12 (both inclusive), then 'in high school' is output.
+#Otherwise, 'not in high school' is output.
+#Ex 1: If the input is 10, then the output is:
+#in high school
+#Ex 2: If the input is 8, then the output is:
+#not in high school
+#Note: 0 < x < 5 evaluates to true if x is between the ranges 0 and 5 (both non-inclusive).
+user_grade = int(input())
+
+if 12>user_grade>9:
+    print('in high school')
+else:
+    print('not in high school')
+'''
+
+
