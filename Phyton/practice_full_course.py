8265709ba8adb885378fd16cad8214ccf729cd24
Miguel Gonzalez <miguelgonzalez404@outlook.com>
2025-12-16 21:03:39 -0600

    while loop

--- Phyton/practice_full_course.py
+++ Phyton/practice_full_course.py
@@ -1793,3 +1793,47 @@ while user_num >= 0:
 
 print(f'Output number is {output_num}')
 '''
+
+'''#Greatest common divisor
+num_a = int(input('Enter first positive integer: '))
+print()
+
+num_b = int(input('Enter second positive integer: '))
+print()
+
+while num_a != num_b:
+    if num_a > num_b:
+        num_a = num_a - num_b
+    else:
+        num_b = num_b - num_a
+
+print(f'GCD is {num_a}') 
+'''
+
+'''#while loop: Conversation 
+#Program that has a conversation with the user.
+#Uses elif branching and a random number to mix up the program's responses.
+#import random  # Import a library to generate random numbers
+
+print('Tell me something about yourself.')
+print('You can type \'Goodbye\' at anytime to quit.\n')
+
+user_text = input()
+
+while user_text != 'Goodbye':
+    random_num = random.randint(0, 2)  # Gives a random integer between 0 and 2
+    if random_num == 0:
+        print('\nPlease explain further.\n')
+    elif random_num == 1:
+        print(f'\nWhy do you say: "{user_text}"?\n')
+    elif random_num == 2:
+        print('\nWhat else can you share?\n')
+    else:
+        print('\nUh-oh, something went wrong. Try again.\n')
+
+    user_text = input()
+
+print('It was nice talking with you. Goodbye.\n')
+'''
+
+
