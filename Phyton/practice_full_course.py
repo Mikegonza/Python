517847d0b68ac8fc4c2a949052f633b6083c7697
Miguel Gonzalez <miguelgonzalez404@outlook.com>
2026-01-11 14:39:49 -0600

    ok

--- Phyton/practice_full_course.py
+++ Phyton/practice_full_course.py
@@ -2343,3 +2343,20 @@ for (index,value) in enumerate(values_list):
         print(f'Sample {index} is ok')
 '''
 
+'''#Write a program that takes in a positive integer as input, 
+#and outputs a string of 1's and 0's representing the integer in reverse binary. 
+# For an integer x, the algorithm is:
+#As long as x is greater than 0
+   #Output x modulo 2 (remainder is either 0 or 1)
+   #Assign x with x divided by 2
+#Note: The above algorithm outputs the 0's and 1's in reverse order.
+#Ex: If the input is:6
+#the output is:011
+#6 in binary is 110; the algorithm outputs the bits in reverse.
+integer=int(input())
+
+while integer > 0:
+    print(integer % 2, end='')
+    integer= integer // 2
+print()'''
+
