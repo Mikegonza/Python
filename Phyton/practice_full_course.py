a05c9230f3ec74b1e338d861998c39645961e272
Miguel Gonzalez <miguelgonzalez404@outlook.com>
2026-02-09 21:13:47 -0600

    ok

--- Phyton/practice_full_course.py
+++ Phyton/practice_full_course.py
@@ -2478,4 +2478,51 @@ for value in values:
     print(f'{normalized_value:.2f}')
 '''
 
+'''#Numbers program with multiple functions
+size = 5
+
+def get_numbers(num):
+    numbers = []
+    user_input = input(f'Enter {num} integers:\n')
+
+    i = 0
+    for token in user_input.split():
+        number = int(token)     # Convert string input into integer
+        numbers.append(number)  # Add to numbers list
+
+        print(i, number)
+        i += 1
+
+    return numbers
+
+def print_all_numbers(numbers):
+    print('Numbers:', end=' ')
+    for num in numbers:
+        print(num, end=' ')
+    print()
+
+def print_odd_numbers(numbers):
+    # Print all odd numbers
+    print('Odd numbers:', end=' ')
+    for num in numbers:
+        if num % 2 != 0:
+            print(num, end=' ')
+    print()
+
+def print_negative_numbers(numbers):
+    # Print all negative numbers
+    print('Negative numbers:', end=' ')
+    for num in numbers:
+        if num < 0:   # Check if number is negative
+            print(num, end=' ')
+    print()
+
+nums = get_numbers(size)
+print_all_numbers(nums)
+print_odd_numbers(nums)
+print_negative_numbers(nums)
+'''
+
+
+
 
