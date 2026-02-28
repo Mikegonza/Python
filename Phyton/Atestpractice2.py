7b6066e5d00303f4c51bbc5b058f67665f29e5d2
Mikegonza <miguelangel.gonzalezmaur@my.tccd.edu>
2026-02-28 00:13:17 -0600

    ok

---
+++ Phyton/Atestpractice2.py
@@ -0,0 +1,10 @@
+start_index = int(input())
+end_index = int(input())
+my_string = 'Jack be nimble, Jack be quick'
+
+''' Your code goes here '''
+my_str=my_string[start_index:end_index]
+sliced_lyric=my_str
+
+print('Full lyric:', my_string)
+print('Sliced lyric:', sliced_lyric)
