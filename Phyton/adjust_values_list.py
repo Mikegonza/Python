9c1f8186eb9f5d868b1fffca190cd6c4912cd9a5
Mikegonza <miguelangel.gonzalezmaur@my.tccd.edu>
2026-04-05 16:39:57 -0500

    ok

--- Phyton/adjust_values_list.py
+++ Phyton/adjust_values_list.py
@@ -1,7 +1,7 @@
-'''Adjust values in a list by normalizing
-When analyzing data sets, such as data for human heights or for human weights, 
+"""Adjust values in a list by normalizing
+When analyzing data sets, such as data for human heights or for human weights,
 a common step is to adjust the data. This adjustment can be done by normalizing to values between 0 and 1, or throwing away outliers.
-For this program, adjust the values by dividing all values by the largest value. 
+For this program, adjust the values by dividing all values by the largest value.
 The input begins with an integer indicating the number of floating-point values that follow.
 Output each floating-point value with two digits after the decimal point, which can be achieved as follows:
 print(f'{your_value:.2f}')
@@ -18,8 +18,9 @@ the output is:
 0.10
 1.00
 0.65
-The 5 indicates that there are five floating-point values in the list, 
-namely 30.0, 50.0, 10.0, 100.0, and 65.0. 100.0 is the largest value in the list, so each value is divided by 100.0.'''
+The 5 indicates that there are five floating-point values in the list,
+namely 30.0, 50.0, 10.0, 100.0, and 65.0. 100.0 is the largest value in the list, so each value is divided by 100.0.
+"""
 
 num_values = int(input("Enter the number of values: "))
 
@@ -32,4 +33,4 @@ max_value = max(values)
 
 for value in values:
     normalized_value = value / max_value
-    print(f'{normalized_value:.2f}')
+    print(f"{normalized_value:.2f}")
