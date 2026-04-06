87692248b1e9c303cf1ce8d4fa20e3847567fb37
Mikegonza <miguelangel.gonzalezmaur@my.tccd.edu>
2026-04-05 20:08:53 -0500

    ok

--- Phyton/apractice.py
+++ Phyton/apractice.py
@@ -1,2 +1,16 @@
-s = "banana"
-print(s[::2])
+# s = "banana"
+# print(s[::2])
+
+not_on_board = input().split()
+on_board_bus = input().split()
+
+print(f"Passengers waiting at a bus stop: {not_on_board}")
+print(f"Passengers on board: {on_board_bus}")
+
+""" Your code goes here """
+left_passenger = not_on_board.pop()
+print(f"Passenger left bus stop:{left_passenger}")
+on_board_bus.extend(not_on_board)
+
+
+print(f"Updated passengers on board: {on_board_bus}")
